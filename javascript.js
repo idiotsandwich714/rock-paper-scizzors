546fac73be2773b8286c51a8cbc1edf12df0a6ff
Daniel Tan <122583737+idiotsandwich714@users.noreply.github.com>
2023-02-03 15:49:01 -0800

    Implement function that prompts for the player's selection

--- javascript.js
+++ javascript.js
@@ -1,14 +1,33 @@
+// Paper beats Rock, Scissors beats Paper, Rock beats Scissors
+// 1 beats 0, 2 beats 1, 0 beats 2
+
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
+    const computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0) {
-        console.log("Rock");
+        console.log("Computer Selection: Rock");
     } else if (computerChoice === 1) {
-        console.log("Paper");
+        console.log("Computer Selection: Paper");
     } else {
-        console.log ("Scizzors");
-    }
+        console.log ("Computer Selection: Scissors");
+    } 
 }
 
 getComputerChoice();
 
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
+    let playerSelection;
+
+    if (playerChoice === "rock") {
+        playerSelection = 0
+    } else if (playerChoice === "paper") {
+        playerSelection = 1
+    } else if (playerChoice === "scissors") {
+        playerSelection = 2
+    }    
+
+    console.log("Player Selection: " + playerSelection);
+}
+
+getPlayerChoice();
