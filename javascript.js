a8bf1bbc2b775e1d416dd4e6c2877ed993a27c45
Daniel Tan <122583737+idiotsandwich714@users.noreply.github.com>
2023-02-04 20:37:28 -0800

    Implement round simulation and win, lose, or tie scenarios

--- javascript.js
+++ javascript.js
@@ -1,33 +1,34 @@
-// Paper beats Rock, Scissors beats Paper, Rock beats Scissors
-// 1 beats 0, 2 beats 1, 0 beats 2
+const computerChoice = Math.floor(Math.random() * 3);
 
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3);
-
     if (computerChoice === 0) {
-        console.log("Computer Selection: Rock");
+        return "rock";
     } else if (computerChoice === 1) {
-        console.log("Computer Selection: Paper");
+        return "paper";
     } else {
-        console.log ("Computer Selection: Scissors");
-    } 
+        return "scissors";
+    }
+    
 }
 
-getComputerChoice();
-
-function getPlayerChoice() {
-    let playerChoice = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
-    let playerSelection;
-
-    if (playerChoice === "rock") {
-        playerSelection = 0
-    } else if (playerChoice === "paper") {
-        playerSelection = 1
-    } else if (playerChoice === "scissors") {
-        playerSelection = 2
-    }    
-
-    console.log("Player Selection: " + playerSelection);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "tie game";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "you won";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "you won";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "you won";
+    } else {
+        return "loser";
+    }
 }
 
-getPlayerChoice();
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+
+console.log("Player : " + playerSelection);
+console.log("Computer : " + computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
