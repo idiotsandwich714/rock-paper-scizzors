c443eb26dad212b673cde069b17d910ad7fa4c29
Daniel Tan <122583737+idiotsandwich714@users.noreply.github.com>
2023-02-12 17:50:29 -0800

    Implement divs that display each round's player and computer selections, score, and result

--- javascript.js
+++ javascript.js
@@ -15,26 +15,34 @@ function getComputerChoice() {
     }
 }
 
+const playerWeapon = document.querySelector("#playerWeapon");
+const computerWeapon = document.querySelector("#computerWeapon");
+const playerScoreBoard = document.querySelector("#playerScoreBoard");
+const computerScoreBoard = document.querySelector("#computerScoreBoard");
+const roundResult = document.querySelector("#roundResult");
+
 function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
-    console.log("Computer Selection: " + computerSelection);
-    console.log("Player Selection: " + playerSelection);
-    console.log("Computer Score: " + computerScore);
-    console.log("Player Score: " + playerScore);
+
+    playerWeapon.textContent = "Player Selection: " + playerSelection;
+    computerWeapon.textContent = "Computer Selection: " + computerSelection;
+    playerScoreBoard.textContent = "Player Score: " + playerScore;
+    computerScoreBoard.textContent = "Computer Score: " + computerScore;
+    
     if (playerSelection === computerSelection) {
-        return tie;
+        roundResult.textContent = tie;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++;
-        return win;
+        roundResult.textContent = win;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++;
-        return win;
+        roundResult.textContent = win;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore++;
-        return win;
+        roundResult.textContent = win;
     } else {
         computerScore++;
-        return lose;
+        roundResult.textContent = lose;
     }
 }
 
@@ -45,6 +53,9 @@ const scissors = document.querySelector("#scissors");
 rock.addEventListener("click", () => playRound("rock"));
 paper.addEventListener("click", () => playRound("paper"));
 scissors.addEventListener("click", () => playRound("scissors"));
+
+
+
 
 
 /*
