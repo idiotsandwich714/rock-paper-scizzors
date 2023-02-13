8db2778232b3120ba664ae2c46d3289c21c71fa5
Daniel Tan <122583737+idiotsandwich714@users.noreply.github.com>
2023-02-13 00:11:38 -0800

    Implement racing themed user interface

--- javascript.js
+++ javascript.js
@@ -15,13 +15,7 @@ function getComputerChoice() {
     }
 }
 
-const playerWeapon = document.querySelector("#playerWeapon");
-const computerWeapon = document.querySelector("#computerWeapon");
-const playerScoreBoard = document.querySelector("#playerScoreBoard");
-const computerScoreBoard = document.querySelector("#computerScoreBoard");
 const roundResult = document.querySelector("#roundResult");
-const gameResult = document.querySelector("#gameResult");
-
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
@@ -34,53 +28,87 @@ scissors.addEventListener("click", () => playRound("scissors"));
 function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
 
-    playerWeapon.textContent = "Player Selection: " + playerSelection;
-    computerWeapon.textContent = "Computer Selection: " + computerSelection;
-    
-    
+    if (playerSelection === "rock") {
+        document.getElementById("turtleSelectionImage").src="./images/rock.jpg";
+    } else if (playerSelection === "paper") {
+        document.getElementById("turtleSelectionImage").src="./images/paper.jpg";
+    } else {
+        document.getElementById("turtleSelectionImage").src="./images/scissors.jpg";
+    }
+
+    if (computerSelection === "rock") {
+        document.getElementById("hareSelectionImage").src="./images/rock.jpg";
+    } else if (computerSelection === "paper") {
+        document.getElementById("hareSelectionImage").src="./images/paper.jpg";
+    } else {
+        document.getElementById("hareSelectionImage").src="./images/scissors.jpg"
+    }
+        
     if (playerSelection === computerSelection) {
         roundResult.textContent = tie;
-        playerScoreBoard.textContent = "Player Score: " + playerScore;
-        computerScoreBoard.textContent = "Computer Score: " + computerScore;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++;
         roundResult.textContent = win;
-        playerScoreBoard.textContent = "Player Score: " + playerScore;
-        computerScoreBoard.textContent = "Computer Score: " + computerScore;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++;
         roundResult.textContent = win;
-        playerScoreBoard.textContent = "Player Score: " + playerScore;
-        computerScoreBoard.textContent = "Computer Score: " + computerScore;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore++;
         roundResult.textContent = win;
-        playerScoreBoard.textContent = "Player Score: " + playerScore;
-        computerScoreBoard.textContent = "Computer Score: " + computerScore;
     } else {
         computerScore++;
         roundResult.textContent = lose;
-        playerScoreBoard.textContent = "Player Score: " + playerScore;
-        computerScoreBoard.textContent = "Computer Score: " + computerScore;
+    }
+
+    if (playerScore === 1) {
+        document.getElementById("turtleroadone").src="./images/mexicanturtle.jpg";
+    } else if (playerScore === 2) {
+        document.getElementById("turtleroadone").src="./images/road.png";
+        document.getElementById("turtleroadtwo").src="./images/mexicanturtle.jpg";
+    } else if (playerScore === 3) {
+        document.getElementById("turtleroadtwo").src="./images/road.png";
+        document.getElementById("turtleroadthree").src="./images/mexicanturtle.jpg";
+    } else if (playerScore === 4) {
+        document.getElementById("turtleroadthree").src="./images/road.png";
+        document.getElementById("turtleroadfour").src="./images/mexicanturtle.jpg";
+    } else if (playerScore === 5) {
+        document.getElementById("turtleroadfour").src="./images/road.png";
+        document.getElementById("turtleroadfive").src="./images/mexicanturtle.jpg";
+    }
+
+    if (computerScore === 1) {
+        document.getElementById("hareroadone").src="./images/mexicanrabbit.jpg";
+    } else if (computerScore === 2) {
+        document.getElementById("hareroadone").src="./images/road.png";
+        document.getElementById("hareroadtwo").src="./images/mexicanrabbit.jpg";
+    } else if (computerScore === 3) {
+        document.getElementById("hareroadtwo").src="./images/road.png";
+        document.getElementById("hareroadthree").src="./images/mexicanrabbit.jpg";
+    } else if (computerScore === 4) {
+        document.getElementById("hareroadthree").src="./images/road.png";
+        document.getElementById("hareroadfour").src="./images/mexicanrabbit.jpg";
+    } else if (computerScore === 5) {
+        document.getElementById("hareroadfour").src="./images/road.png";
+        document.getElementById("hareroadfive").src="./images/mexicanrabbit.jpg";
     }
 
     if (playerScore === 5) {
-        gameResult.textContent = "Game Over: Player Wins the Game!"
+        roundResult.textContent = "Game Over: Player Wins the Game!"
         document.getElementById("rock").disabled = true;
         document.getElementById("paper").disabled = true;
         document.getElementById("scissors").disabled = true;
         const reset = document.createElement("button");
         reset.textContent = "Play Again";
-        document.body.appendChild(reset);
+        document.getElementById("roundResult").appendChild(reset);
         reset.addEventListener("click", () => window.location.reload());
     } else if (computerScore === 5) {
-        gameResult.textContent = "Game Over: Player Wins the Game!"
+        roundResult.textContent = "Game Over: Player Wins the Game!"
         document.getElementById("rock").disabled = true;
         document.getElementById("paper").disabled = true;
         document.getElementById("scissors").disabled = true;
         const reset = document.createElement("button");
         reset.textContent = "Play Again";
-        document.body.appendChild(reset);
+        document.getElementById("roundResult").appendChild(reset);
         reset.addEventListener("click", () => window.location.reload());
     }
 }
